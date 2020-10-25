526fac393ad981337ca3a63b3154c562747575b3
dscol <dscolby17@gmail.com>
2020-10-25 18:07:59 -0400

    Packaged into wheel file

--- setup.py
+++ setup.py
@@ -15,12 +15,11 @@ setuptools.setup(
                      "statistics and generate bar plots of missingness",
     long_description_content_type="text/markdown",
     url="https://github.com/dscolby/clearmatch",
-    packages=setuptools.find_packages(),
+    packages=['clearmatch', 'test'],
     install_requires=['matplotlib', 'pandas', 'numpy'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    ]
 )
